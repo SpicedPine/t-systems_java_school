effe8946d2af73e6d253fae391e600500743855d
Igor <igor_noskov_a@mail.ru>
2021-01-25 03:46:37 +0300

    Сервис без доп методов относительно Dao для ProcAndMedDAO

---
+++ service/src/main/java/com/noskov/school/service/api/ProcAndMedService.java
@@ -0,0 +1,15 @@
+package com.noskov.school.service.api;
+
+import com.noskov.school.persistent.ProcedureAndMedicinePO;
+
+import java.util.List;
+
+public interface ProcAndMedService {
+    List<ProcedureAndMedicinePO> getAllProceduresAndMedicines();
+    List<ProcedureAndMedicinePO> getAllProcedures();
+    List<ProcedureAndMedicinePO> getAllMedicines();
+    void add(ProcedureAndMedicinePO procedureOrMedicine);
+    ProcedureAndMedicinePO getById(Long id);
+    void delete(ProcedureAndMedicinePO procedureOrMedicine);
+    void update(ProcedureAndMedicinePO procedureOrMedicine);
+}
---
+++ service/src/main/java/com/noskov/school/service/imp/ProcAndMedServiceImp.java
@@ -0,0 +1,51 @@
+package com.noskov.school.service.imp;
+
+import com.noskov.school.dao.api.ProcAndMedDAO;
+import com.noskov.school.persistent.ProcedureAndMedicinePO;
+import com.noskov.school.service.api.ProcAndMedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.Access;
+import java.util.List;
+
+@Service
+public class ProcAndMedServiceImp implements ProcAndMedService {
+    @Autowired
+    ProcAndMedDAO procAndMedDAO;
+
+    @Override
+    public List<ProcedureAndMedicinePO> getAllProceduresAndMedicines() {
+        return procAndMedDAO.getAllProceduresAndMedicines();
+    }
+
+    @Override
+    public List<ProcedureAndMedicinePO> getAllProcedures() {
+        return procAndMedDAO.getAllProcedures();
+    }
+
+    @Override
+    public List<ProcedureAndMedicinePO> getAllMedicines() {
+        return procAndMedDAO.getAllMedicines();
+    }
+
+    @Override
+    public void add(ProcedureAndMedicinePO procedureOrMedicine) {
+        procAndMedDAO.add(procedureOrMedicine);
+    }
+
+    @Override
+    public ProcedureAndMedicinePO getById(Long id) {
+        return procAndMedDAO.getById(id);
+    }
+
+    @Override
+    public void delete(ProcedureAndMedicinePO procedureOrMedicine) {
+        procAndMedDAO.delete(procedureOrMedicine);
+    }
+
+    @Override
+    public void update(ProcedureAndMedicinePO procedureOrMedicine) {
+        procAndMedDAO.update(procedureOrMedicine);
+    }
+}
